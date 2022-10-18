9ad52fa90b431a837f2f281112e13e8a81124092
Adrian Senkevich <adriansenkevich@Adrians-MacBook-Air.local>
2022-10-19 00:56:36 +0300

    third commit

--- src/details.js
+++ src/details.js
@@ -1,14 +1,43 @@
+import { useEffect, useState } from 'react'
 import './styles/details.scss'
 
 function Details(props) {
 
+    const [scale, setScale] = useState('')
+
+    useEffect(() => {
+        console.log(props.element.melt - 273)
+    }, [scale])
+
+    function change() {
+        if(scale == 'Celsius'){
+            return props.element.melt - 273
+        }
+        return props.element.melt
+    }
+
     return (
-        <div className={props.active ? 'main active' : 'main'} onClick={() => props.setActive(false)}>
-            <div className='content'>
-                {props.element.category}<br/>
-                {props.element.name}<br/>
-                {props.element.number}<br/>
-                Atomic mass - {props.element.atomic_mass}<br/>
+        <div className={props.active ? 'main active' : 'main'} onClick={() => props.setActive(!props.active)}>
+            <div className='content' onClick={e => e.stopPropagation()}>
+                <div className='element'>
+                    <div className='symbol' style={{backgroundColor:props.color}}>
+                        {props.element.symbol}
+                    </div>
+                    {props.element.name}                 
+                </div>
+                <div>
+                    Series: {props.element.category}
+                </div>
+                <div>
+                    Melting point: {props.element.name}
+                    <select onChange={e => setScale(e.target.value)}>
+                        <option value='kelvin'>Kelvin</option>
+                        <option value='celsius'>Celsius</option>
+                    </select>
+                </div>
+                <div>
+                    Atomic number: {props.element.number}
+                </div>
             </div>
         </div>
     )
--- src/element.js
+++ src/element.js
@@ -1,53 +1,53 @@
-import React from "react";
-import st from '../src/styles/table.module.scss'
+import React, { useEffect } from "react";
+import st from '../src/styles/element.module.scss'
 import { useState } from "react";
 import Details from "./details";
 
 function Element(props) {
 
     const [active, setActive] = useState(false)
+    const [color, setColor] = useState('')
     
     function whichColor(element) {
         switch (element.category) {
             case 'diatomic nonmetal':
-                return 'rgb(56, 245, 144)'
+                return 'rgba(56, 245, 144, 0.5)'
             case 'noble gas':
-                return 'rgb(240, 77, 255)'
+                return 'rgba(240, 77, 255, 0.5)'
             case 'alkali metal':
-                return 'rgb(250, 219, 67)'
+                return 'rgba(250, 219, 67, 0.5)'
             case 'alkaline earth metal':
-                return 'rgb(221, 255, 0)'
+                return 'rgba(221, 255, 0, 0.5)'
             case 'metalloid':
-                return 'rgb(87, 231, 250)'
+                return 'rgba(87, 231, 250, 0.5)'
             case 'polyatomic nonmetal':
-                return 'rgb(247, 106, 139)'
+                return 'rgba(247, 106, 139, 0.5)'
             case 'post-transition metal':
-                return 'rgb(35, 196, 217)'
+                return 'rgba(35, 196, 217, 0.5)'
             case 'transition metal':
-                return 'rgb(247, 183, 183)'
+                return 'rgba(247, 183, 183, 0.5)'
             case 'lanthanide':
-                return 'rgb(247, 182, 106)'
+                return 'rgba(247, 182, 106, 0.5)'
             case 'actinide':
-                return 'rgb(248, 173, 255)'
+                return 'rgba(248, 173, 255, 0.5)'
             default:
-                return 'rgb(212, 212, 212)'
+                return 'rgba(212, 212, 212, 0.5)'
         }
     }
 
-    function exit(){
-        console.log(1)
-        setActive(true)
+    function exit() {
+        setActive(!active)
+        setColor(whichColor(props.element))
     }
 
     return (
-        <div className={st.element} style={{gridColumn:props.element.xpos, gridRow:props.element.ypos, backgroundColor:whichColor(props.element)}}>
+        <div onClick={exit} className={st.element} style={{gridColumn:props.element.xpos, gridRow:props.element.ypos, backgroundColor:whichColor(props.element)}}>
             {props.element.number}<br/>
             <b>{props.element.symbol}</b>
             <div>
                 {props.element.name}
             </div>
-            <button onClick={() => setActive(true)}>Info</button>
-            <Details active={active} setActive={setActive} element={props.element}></Details>
+            <Details active={active} setActive={setActive} element={props.element} color={color}></Details>
         </div>
     )
 }
