2a4998e10c77b4e9cb8c92ebb25593b435508983
Adrian Senkevich <adriansenkevich@Adrians-MacBook-Air.local>
2022-10-26 00:07:21 +0200

    grid improvements

--- src/components/details.js
+++ src/components/details.js
@@ -21,18 +21,37 @@ function Details(props) {
                     </div>
                     {props.element.name}                 
                 </div>
-                <div>
-                    Series: {props.element.category}
+                <div className='description'>
+                     <div>
+                        Series: {props.element.category}
+                    </div>
+                    <div>
+                        Melting point: {melt}
+                        <select onChange={e => setScale(e.target.value)}>
+                            <option value='kelvin'>Kelvin</option>
+                            <option value='celsius'>Celsius</option>
+                        </select>
+                    </div>
+                    <div>
+                        Atomic number: {props.element.number}
+                    </div>
+                    <div>
+                        atomic_mass
+                    </div>
+                    <div>
+                        period
+                    </div>
+                    <div>
+                        electron_configuration
+                    </div>
                 </div>
-                <div>
-                    Melting point: {melt}
-                    <select onChange={e => setScale(e.target.value)}>
-                        <option value='kelvin'>Kelvin</option>
-                        <option value='celsius'>Celsius</option>
-                    </select>
-                </div>
-                <div>
-                    Atomic number: {props.element.number}
+                <div className='classify'>
+                    <div>1:</div>
+                    <div>2:</div>
+                    <div>3:</div>
+                    <div>4:</div>
+                    <div>5:</div>
+                    <div>6:</div>
                 </div>
             </div>
         </div>
